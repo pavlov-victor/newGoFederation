97b384cb6dff4352507de8f2be6c7b39d7c219a1
viktor <vitek@goykt.ru>
2022-03-02 13:26:41 +0900

    add login page

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'users/login.html')
+
+    def post(self, request, *args, **kwargs):
+        return render(request, 'users/login.html')
